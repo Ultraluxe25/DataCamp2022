6827f7cb0e34d6d628971e3f57c9d336120af76c
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-07-15 10:22:05 +0300

    adding "drop table"

--- unit_1/lab_3/task_1.sql
+++ unit_1/lab_3/task_1.sql
@@ -1,6 +1,5 @@
-create tablespace tbs_lab datafile 'db_lab_001.dat' size 5M autoextend ON next 5M MAXSIZE 100M; 
-GRANT UNLIMITED TABLESPACE TO AKapturov;
-
+create tablespace tbs_lab datafile 'db_lab_002.dat' size 5M autoextend ON next 5M MAXSIZE 100M; 
+GRANT UNLIMITED TABLESPACE TO SNasekajlo;
 create table t
     (a int,
      b varchar2 (4000) default rpad ('*',4000,'*'),
@@ -8,13 +7,17 @@ create table t
      )
      /
      
- insert into t (a) values (1);
- insert into t (a) values (2);
- insert into t (a) values (3);
- commit;
- delete from t where a=2;
- commit;
- insert into t (a) values (4);
- commit;
- 
- select a from t
+insert into t (a) values (1);
+insert into t (a) values (2);
+insert into t (a) values (3);
+commit;
+
+delete from t where a=2;
+commit;
+
+insert into t (a) values (4);
+commit;
+
+select a from t
+/
+drop table t;
