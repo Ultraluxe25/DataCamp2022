0554c4d14d0322c30ccff750191ee93158650e63
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-07-20 14:34:46 +0300

    Tried to understand and evolve task 2

--- unit_1/lab_3/task_2.sql
+++ unit_1/lab_3/task_2.sql
@@ -1,14 +1,21 @@
-create table t (x int primary key, y clob, z blob) SEGMENT CREATION IMMEDIATE / 
-select 
-  DBMS_METADATA.GET_DDL('TABLE', 'T') 
-from 
-  dual;
-create table t
-    (x int primary key,
-     y clob,
-     z blob)
-SEGMENT CREATION IMMEDIATE 
-/
-select DBMS_METADATA.GET_DDL('TABLE','T') from dual;
+/* Here big mistake that make problems
+ORA-00054: ресурс занят и задано его получение с параметром NOWAIT,
+либо истекло время ожидания
+I tried to solve it but I didn't  */
+
+Create table t ( x int primary key, y clob, z blob );
+
+select segment_name, segment_type from user_segments;
 
 drop table t;
+
+Create table t 
+    ( x int primary key, 
+      y clob, 
+      z blob ) 
+SEGMENT CREATION IMMEDIATE
+/
+
+select segment_name, segment_type from user_segments;
+
+SELECT DBMS_METADATA.GET_DDL('TABLE','T') FROM dual
