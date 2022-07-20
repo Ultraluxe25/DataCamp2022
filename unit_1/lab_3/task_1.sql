0e3c0faaa811676bb50ad07415b1b78634435141
Kapturov Alexander <86742866+Ultraluxe25@users.noreply.github.com>
2022-07-20 12:38:26 +0300

    decided to resolve my lab

--- unit_1/lab_3/task_1.sql
+++ unit_1/lab_3/task_1.sql
@@ -1,25 +1,30 @@
-create tablespace tbs_lab datafile 'db_lab_001.dat' size 5M autoextend ON next 5M MAXSIZE 100M; 
+/* When your new user is created, you can get ” ORA-01950: no privileges
+on tablespace ” error during Insert operation. */
 GRANT UNLIMITED TABLESPACE TO AKapturov;
 
 create table t
-    (a int,
-     b varchar2 (4000) default rpad ('*',4000,'*'),
-     c varchar2 (3000) default rpad ('*',3000,'*')
-     )
-     /
-     
-insert into t (a) values (1);
-insert into t (a) values (2);
-insert into t (a) values (3);
+  ( a int,
+    b varchar2(4000) default rpad('*',4000,'*'),  -- there will be 4000 '*'
+    c varchar2(3000) default rpad('*',3000,'*')
+   );
+
+insert into t (a) values ( 1);
+insert into t (a) values ( 2);
+insert into t (a) values ( 3);
 commit;
 
-delete from t where a=2;
+select * from t;
+
+delete from t where a = 2 ;
 commit;
 
-insert into t (a) values (4);
+select * from t;
+
+insert into t (a) values ( 4);
 commit;
 
-select a from t
-/
+select a from t;
 
 drop table t;
+
+purge recyclebin;
